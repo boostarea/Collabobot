413954aa0bbafabfd69d97ba153c38ee03b4f1b3
Frankzhaopku <syzhao1988@126.com>
2019-01-11 12:45:16 +0800

    typo fix

--- common_tools/check.sh
+++ common_tools/check.sh
@@ -5,5 +5,5 @@ find . -path ./node_modules -prune -o -name '*' -type f -print0 | xargs -0 ./com
 echo 'Running markdownlint check.'
 find . -path ./node_modules -prune -o -name '*.md' -type f -print0 | xargs -0 markdownlint --config ./common_tools/markdownlint.json
 
-echo 'Running markdown-link-chech.'
+echo 'Running markdown-link-check.'
 find . -path ./node_modules -prune -o -name '*.md' -type f -print0 | xargs -0 markdown-link-check -q
